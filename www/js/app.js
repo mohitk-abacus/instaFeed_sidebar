50e1484a1dae2a628cd1d5df56f704156fe27125
mohitk-abacus <mohitk@abacusdesk.co.in>
2017-08-26 09:53:35 +0530

    instafeed

--- www/js/app.js
+++ www/js/app.js
@@ -9,39 +9,39 @@ angular.module('starter', ['ionic', 'starter.controllers'])
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'AppCtrl'
-  })
-
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
+        
       }
-    }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleDefault();
+      }
+    });
   })
-
-  .state('app.browse', {
+  
+  .config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+    
+    .state('app', {
+      url: '/app',
+      abstract: true,
+      templateUrl: 'templates/menu.html',
+      controller: 'AppCtrl'
+    })
+    
+    .state('app.search', {
+      url: '/search',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/search.html'
+        }
+      }
+    })
+    
+    .state('app.browse', {
       url: '/browse',
       views: {
         'menuContent': {
@@ -58,16 +58,202 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         }
       }
     })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
+    
+    .state('app.single', {
+      url: '/playlists/:playlistId',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/playlist.html',
+          controller: 'PlaylistCtrl'
+        }
       }
-    }
+    })
+    
+    
+    
+    .state('intro', {
+      url: '/intro',
+      cache: false,
+      templateUrl: 'templates/introduction.html'
+    })
+      
+
+    .state('app.newslisting', {
+      url: '/newslisting',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/newslisting.html',
+          controller: 'PlaylistCtrl'
+        }
+      }
+    })
+     
+
+    .state('app.talentslisting', {
+      url: '/talentslisting',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/talentslisting.html',
+          controller: 'PlaylistCtrl'
+        }
+      }
+    })
+     
+
+    .state('app.explorelisting', {
+      url: '/explorelisting',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/explorelisting.html',
+          controller: 'PlaylistCtrl'
+        }
+      }
+    })
+     
+    
+
+    .state('app.listingdetail', {
+      url: '/listingdetail',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/listingdetail.html',
+          controller: 'PlaylistCtrl'
+        }
+      }
+    })
+     
+ 
+       
+    .state('app.newsupload', {
+      url: '/newsupload',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/newsupload.html',
+          controller: 'PlaylistCtrl'
+        }
+      }
+    })
+
+ 
+       
+    .state('app.profile', {
+      url: '/profile',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/profile.html',
+          controller: 'PlaylistCtrl'
+        }
+      }
+    })
+
+       
+    .state('app.editprofile', {
+      url: '/editprofile',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/editprofile.html',
+          controller: 'PlaylistCtrl'
+        }
+      }
+    })
+
+
+       
+    .state('app.mypost', {
+      url: '/mypost',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/mypost.html',
+          controller: 'PlaylistCtrl'
+        }
+      }
+    })
+
+       
+    .state('app.userprofile', {
+      url: '/userprofile',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/userprofile.html',
+          controller: 'PlaylistCtrl'
+        }
+      }
+    })
+
+       
+    .state('app.following', {
+      url: '/following',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/following.html',
+          controller: 'PlaylistCtrl'
+        }
+      }
+    })
+       
+    .state('app.followers', {
+      url: '/followers',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/followers.html',
+          controller: 'PlaylistCtrl'
+        }
+      }
+    })
+
+       
+    .state('app.petitionslisting', {
+      url: '/petitionslisting',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/petitionslisting.html',
+          controller: 'PlaylistCtrl'
+        }
+      }
+    })
+       
+    .state('app.petitiondetail', {
+      url: '/petitiondetail',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/petitiondetail.html',
+          controller: 'PlaylistCtrl'
+        }
+      }
+    })
+
+       
+    .state('app.pollslisting', {
+      url: '/pollslisting',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/pollslisting.html',
+          controller: 'PlaylistCtrl'
+        }
+      }
+    })
+
+       
+    .state('app.pollsdetail', {
+      url: '/pollsdetail',
+      views: {
+        'menuContent': {
+          templateUrl: 'templates/pollsdetail.html',
+          controller: 'PlaylistCtrl'
+        }
+      }
+    });
+
+
+  // .state('newsupload', {
+  //   url: '/newsupload',
+  //   templateUrl: 'templates/newsupload.html',
+  //   controller: 'PlaylistCtrl'
+  // });
+
+
+    
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/intro');
   });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-});
+  
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,56 +1,258 @@
 angular.module('starter.controllers', [])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
-
+  
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
   // To listen for when this page is active (for example, to refresh data),
   // listen for the $ionicView.enter event:
   //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    //});
+    
+    // Form data for the login modal
+    // $scope.loginData = {};
+    
 
-  // Form data for the login modal
-  $scope.loginData = {};
 
-  // Create the login modal that we will use later
-  $ionicModal.fromTemplateUrl('templates/login.html', {
-    scope: $scope
-  }).then(function(modal) {
-    $scope.modal = modal;
+
+    
+// ========================
+// ======================== LOGIN TEMPLATES MODAL FUNCTION START
+// ========================
+
+    // Create the login modal that we will use later
+    $ionicModal.fromTemplateUrl('templates/login.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.modal = modal;
+    });
+    
+    // Open the LOGIN modal
+    $scope.login = function() {
+      $scope.modal.show();
+    };
+    
+    // Triggered in the forgot password modal to close it
+    $scope.closelogin = function() {
+      $scope.modal.hide();
+    };
+// ========================
+// ======================== LOGIN TEMPLATES MODAL FUNCTION END
+// ========================
+// -----------
+// -----------
+// -----------
+// -----------
+// ========================
+// ======================== FORGOT PASSWORD TEMPLATES MODAL FUNCTION START
+// ========================
+    
+    $ionicModal.fromTemplateUrl('templates/forgot-password.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.forgotpasswordmodal = modal;
+    });
+    
+    // Open the forgot password modal
+    $scope.forgotpassword = function() {
+      $scope.forgotpasswordmodal.show();
+    };
+    
+    // Triggered in the forgot password modal to close it
+    $scope.closeforgotpassword = function() {
+      $scope.forgotpasswordmodal.hide();
+    };
+
+// ========================
+// ======================== FORGOT PASSWORD TEMPLATES MODAL FUNCTION END
+// ========================
+// -----------
+// -----------
+// -----------
+// ----------- 
+// ========================
+// ======================== SIGN UP TEMPLATES MODAL FUNCTION START
+// ========================
+    
+    $ionicModal.fromTemplateUrl('templates/signup.html', {
+      scope: $scope
+    }).then(function(modal) {
+      $scope.signupmodal = modal;
+    });
+    
+    // Open the SIGN modal
+    $scope.signup = function() {
+      $scope.signupmodal.show();
+    };
+    
+    // Triggered in the forgot password modal to close it
+    $scope.closesignup = function() {
+      $scope.signupmodal.hide();
+    };
+
+// ========================
+// ======================== SIGN UP TEMPLATES MODAL FUNCTION END
+// ========================
+// -----------
+// -----------
+// -----------
+// -----------
+// ========================
+// ======================== CHNAGE PASSWORD TEMPLATES MODAL FUNCTION START
+// ========================
+    
+    $ionicModal.fromTemplateUrl('templates/change-password.html', {
+      scope: $scope
+    })
+    .then(function(modal) {
+      $scope.changepasswordmodal = modal;
+    });
+    
+    // Open the forgot password modal
+    $scope.changepassword = function() {
+      $scope.changepasswordmodal.show();
+    };
+    
+    // Triggered in the forgot password modal to close it
+    $scope.closechangepassword = function() {
+      $scope.changepasswordmodal.hide();
+    };
+
+// ========================
+// ======================== FORGOT PASSWORD TEMPLATES MODAL FUNCTION END
+// ========================  
+    
+    
+  
+  })
+  
+  
+  // .controller('PlaylistsCtrl', function($scope) {
+  //   $scope.playlists = [
+  //     { title: 'Reggae', id: 1 },
+  //     { title: 'Chill', id: 2 },
+  //     { title: 'Dubstep', id: 3 },
+  //     { title: 'Indie', id: 4 },
+  //     { title: 'Rap', id: 5 },
+  //     { title: 'Cowbell', id: 6 }
+  //   ];
+  // })
+  
+  .controller('PlaylistCtrl', function($scope, $stateParams, $ionicActionSheet) {
+    
+    
+    $scope.uploads = function() {
+      // Show the action sheet
+      var hideSheet = $ionicActionSheet.show({
+        buttons: [
+          { text: "<i class='icon ion-camera'></i> Photo"},
+          { text: "<i class='icon ion-ios-videocam'></i> Video"},
+          { text: "<i class='icon ion-mic-a'></i> Audio"},
+          { text: "<i class='icon ion-android-image'></i> Gallery" }
+        ],
+        cancelText: 'Cancel',
+        cancel: function() {
+          // add cancel code..
+        },
+        buttonClicked: function(index) {
+          // return true;
+          
+          if(index === 0){ // Manual Button
+            $scope.addsilverpartner();
+          }
+          else if(index === 1){
+            $scope.addgoldpartner();
+          }
+          
+          return true;
+        }
+      })
+    }
+    
+    
+
+// ========================
+// ======================== PROFILE ICON CHNAGE FUNCTION START
+// ========================
+    $scope.camera_click = function() {
+      
+      // Show the action sheet
+      var hideSheet = $ionicActionSheet.show({
+        buttons: [
+          { text: "<i class='icon ion-camera'></i> Camera"},
+          { text: "<i class='icon ion-android-image'></i> Gallery" }
+        ],
+        cancelText: 'Cancel',
+        cancel: function() {
+          // add cancel code..
+        },
+        buttonClicked: function(index) {
+          // return true;
+          
+          if(index === 0){ // Manual Button
+            $scope.addsilverpartner();
+          }
+          else if(index === 1){
+            $scope.addgoldpartner();
+          }
+          
+          return true;
+        }
+      })
+    }
+    
+    // ========================
+    // ======================== PROFILE ICON CHNAGE FUNCTION END
+    // ========================
+    // -----------
+    // -----------
+    // -----------
+    // -----------
+    // ========================
+    // ======================== TOP UNIVERSAL SEARCH FUNCTION START
+    // ========================
+    $scope.addClass = function(){
+      $('.top-search').addClass("active");
+    };
+    
+    $scope.removeClass = function(){
+      $('.top-search').removeClass("active");
+    };
+    // ========================
+    // ======================== TOP UNIVERSAL SEARCH FUNCTION END
+    // ========================
+    // -----------
+    // -----------
+    // -----------
+    // -----------
+    // ========================
+    // ======================== ADD REPLY FUNCTION START
+    // ========================
+    $scope.addReply = function(){
+      $('#1').addClass("active");
+    };
+    
+    // ========================
+    // ======================== ADD REPLY FUNCTION END
+    // ========================
+    // -----------
+    // -----------
+    // -----------
+    // -----------
+    // ========================
+    // ======================== FLOAT ICON FUNCTION START
+    // ========================
+    $scope.class = "nav";
+    $scope.changeClass = function(){
+      if ($scope.class === "nav")
+        $scope.class = "nav active";
+      else
+        $scope.class = "nav";
+    };
+    // ========================
+    // ======================== FLOAT ICON FUNCTION END
+    // ========================    
+    
+    
   });
-
-  // Triggered in the login modal to close it
-  $scope.closeLogin = function() {
-    $scope.modal.hide();
-  };
-
-  // Open the login modal
-  $scope.login = function() {
-    $scope.modal.show();
-  };
-
-  // Perform the login action when the user submits the login form
-  $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
-
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
-  };
-})
-
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae', id: 1 },
-    { title: 'Chill', id: 2 },
-    { title: 'Dubstep', id: 3 },
-    { title: 'Indie', id: 4 },
-    { title: 'Rap', id: 5 },
-    { title: 'Cowbell', id: 6 }
-  ];
-})
-
-.controller('PlaylistCtrl', function($scope, $stateParams) {
-});
+  
